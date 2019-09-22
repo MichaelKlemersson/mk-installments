145530517abafb4c7826f377f9f1bd0ca37f5dd5
Michael Klemersson <mklemersson.dev@gmail.com>
2019-09-22 08:37:08 +0200

    added store modules

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,16 +1,11 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import installments from './modules/installments';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
+  modules: {
+    installments,
   },
 });
